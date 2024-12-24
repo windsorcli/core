36aa9ccd14e9d22978625df31330ce25bf7ae133
Todd VanGundy <todd@windsor-hotel.io>
2024-12-23 22:43:34 -0500

    terraform fmt

--- terraform/cluster/talos/modules/machine/main.tf
+++ terraform/cluster/talos/modules/machine/main.tf
@@ -6,7 +6,7 @@
 terraform {
   required_providers {
     talos = {
-      source = "siderolabs/talos"  # Specify the source for the Talos provider
+      source = "siderolabs/talos" # Specify the source for the Talos provider
     }
   }
 }
@@ -29,11 +29,11 @@ locals {
       # Include install block only if disk_selector is not null
       var.disk_selector != null ? {
         install = {
-          diskSelector    = var.disk_selector  # Disk selector to use for the machine
-          wipe            = var.wipe_disk  # Whether to wipe the disk before installation
-          extraKernelArgs = var.extra_kernel_args  # Additional kernel arguments
-          image           = var.image  # Image to be used for installation
-          extensions      = var.extensions  # Extensions to be used for installation
+          diskSelector    = var.disk_selector     # Disk selector to use for the machine
+          wipe            = var.wipe_disk         # Whether to wipe the disk before installation
+          extraKernelArgs = var.extra_kernel_args # Additional kernel arguments
+          image           = var.image             # Image to be used for installation
+          extensions      = var.extensions        # Extensions to be used for installation
         }
       } : {}
     )
@@ -48,21 +48,21 @@ locals {
 
 # Data source to generate the machine configuration for Talos
 data "talos_machine_configuration" "this" {
-  cluster_name       = var.cluster_name  # Name of the cluster
-  machine_type       = var.machine_type  # Type of the machine (e.g., controlplane, worker)
-  cluster_endpoint   = var.cluster_endpoint  # Endpoint for the cluster API
-  machine_secrets    = var.machine_secrets  # Secrets for the machine
+  cluster_name       = var.cluster_name        # Name of the cluster
+  machine_type       = var.machine_type        # Type of the machine (e.g., controlplane, worker)
+  cluster_endpoint   = var.cluster_endpoint    # Endpoint for the cluster API
+  machine_secrets    = var.machine_secrets     # Secrets for the machine
   kubernetes_version = var.kubernetes_version  # Kubernetes version to be installed
-  talos_version      = "v${var.talos_version}"  # Talos version to be used
-  config_patches     = local.config_patches  # Configuration patches to apply
+  talos_version      = "v${var.talos_version}" # Talos version to be used
+  config_patches     = local.config_patches    # Configuration patches to apply
 }
 
 # Resource to apply the machine configuration to the node
 resource "talos_machine_configuration_apply" "this" {
-  client_configuration        = var.client_configuration  # Client configuration for authentication
-  machine_configuration_input = data.talos_machine_configuration.this.machine_configuration  # Machine configuration data
-  node                        = var.node  # Node identifier for the machine
-  endpoint                    = var.endpoint  # Endpoint for the machine
+  client_configuration        = var.client_configuration                                    # Client configuration for authentication
+  machine_configuration_input = data.talos_machine_configuration.this.machine_configuration # Machine configuration data
+  node                        = var.node                                                    # Node identifier for the machine
+  endpoint                    = var.endpoint                                                # Endpoint for the machine
 }
 
 // Bootstrap the first control plane node
