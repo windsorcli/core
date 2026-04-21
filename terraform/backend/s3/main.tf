9dcef57e7feb09295f1f2f9051e0665cd4ff954f
Ryan VanGundy <85766511+rmvangun@users.noreply.github.com>
2026-04-21 13:32:55 -0400

    Pin terraform version

--- terraform/backend/s3/main.tf
+++ terraform/backend/s3/main.tf
@@ -6,6 +6,7 @@
 #---------------------------------------------------------------------------------------------------
 
 terraform {
+  required_version = ">= 1.10"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
