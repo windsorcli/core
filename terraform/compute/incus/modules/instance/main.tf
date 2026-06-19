000983edfa2a757de3cfbca7b62e0054efb322a6
Ryan VanGundy <85766511+rmvangun@users.noreply.github.com>
2026-06-19 08:44:21 -0400

    chore(terraform): update required Terraform version to >= 1.12.2 across multiple modules and add hyperv provider

--- terraform/compute/incus/modules/instance/main.tf
+++ terraform/compute/incus/modules/instance/main.tf
@@ -3,7 +3,7 @@
 #-----------------------------------------------------------------------------------------------------------------------
 
 terraform {
-  required_version = "~> 1.12.0"
+  required_version = ">= 1.12.2"
   required_providers {
     incus = {
       source  = "lxc/incus"
--- terraform/network/azure-vnet/main.tf
+++ terraform/network/azure-vnet/main.tf
@@ -3,7 +3,7 @@
 #---------------------------------------------------------------------------------------------------
 
 terraform {
-  required_version = "~> 1.12.0"
+  required_version = ">= 1.12.2"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
